0086e4786561c1cefcf153602aa2f919d43a6c2f
Ahmed Ashour <ahmed.ashour@trianglz.com>
2022-12-08 13:23:31 +0200

    Solve day 1 challenge

---
+++ src/day1/Day01.kt
@@ -0,0 +1,14 @@
+package day1
+
+import readInput
+
+fun main() {
+    val input = readInput("input").toString()
+    val result = input.trim { it == '[' || it == ']'}.split(", , ").map { it.split(", ") }.map { it.sumOf { it.toInt() } }.sortedDescending()
+
+    // Part 1
+    println(result[0])
+
+    // Part 2
+    println(result.take(3).sum())
+}
