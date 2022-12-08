02e8e9dbdbfb637c0c7044ac5e5ee8fce8295814
Ahmed Ashour <ahmed.ashour@trianglz.com>
2022-12-08 23:10:10 +0200

    Add path parameter to readInput method

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -3,7 +3,7 @@ package day1
 import readInput
 
 fun main() {
-    val input = readInput("input").toString()
+    val input = readInput("src/day1","input").toString()
     val result = input.trim { it == '[' || it == ']'}.split(", , ").map { it.split(", ") }.map { it.sumOf { it.toInt() } }.sortedDescending()
 
     // Part 1
